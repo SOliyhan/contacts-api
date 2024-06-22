fcf0193451ff4da505c3db967416b6871aaa6de3
Shivanshu <craziest.oliyhan13@gmail.com>
2024-06-22 23:45:47 +0530

    update server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit: "16kb", extended: true}))
 
+app.get("/", (req, res)=> {
+    res.send("Welcome to Contacts-API, follow API documentation for more details.")
+});
+
 app.use("/api/contacts", contactRoute)
 app.use("/api/users", userRoute)
 app.use(errorHandler)
